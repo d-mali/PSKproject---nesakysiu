b81be0f2541041a05e038a8c49e4cfb938c5e8e6
KasparasCiurlionis <kasparasciurlionis@gmail.com>
2024-05-24 16:58:27 +0300

    logger

--- EventPlanningBackend/Middleware/LoggingMiddleware.cs
+++ EventPlanningBackend/Middleware/LoggingMiddleware.cs
@@ -23,9 +23,9 @@ namespace EventBackend.Middleware
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            var user = context.User;
-            var userId = user.Identity.IsAuthenticated ? "anonymous" : user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var userName = user.Identity.IsAuthenticated ? user.Identity.Name : "anonymous";
+            var user = context.User;  
+            var userId = user.Identity != null && user.Identity.IsAuthenticated ? "anonymous" : user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userName = user.Identity != null && user.Identity.IsAuthenticated ? user.Identity.Name : "anonymous";
             var userRoles = user.Claims.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value).ToList();
 
             _logger.LogInformation("Handling request: {Method} {Path} by User: {UserId}, {UserName} Roles: {UserRoles} Date:{Date}", context.Request.Method, context.Request.Path, userId, userName, string.Join(",", userRoles), DateTime.Now);
