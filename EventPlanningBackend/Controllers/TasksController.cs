b5547217645493ee1b1cf4cce43ecdf4b0beaf62
DeniSav <denisijus@gmail.com>
2024-05-25 21:04:15 +0300

    Update TasksController.cs

--- EventPlanningBackend/Controllers/TasksController.cs
+++ EventPlanningBackend/Controllers/TasksController.cs
@@ -7,7 +7,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EventPlanningBackend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Events/{eventId}/[controller]")]
     [ApiController]
     public class TasksController : ControllerBase
     {
@@ -20,23 +20,23 @@ namespace EventPlanningBackend.Controllers
 
         // GET: api/Tasks
         [HttpGet]
-        public async Task<IActionResult> GetTasks()
+        public async Task<IActionResult> GetTasks(Guid eventId)
         {
             return Ok(await _taskService.GetAllTasksAsync());
         }
 
         // GET: api/Tasks/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetTask(Guid id)
+        public async Task<IActionResult> GetTask(Guid eventId)
         {
-            var entity = await _taskService.GetTaskByIdAsync(id);
+            var entity = await _taskService.GetTaskByIdAsync(eventId);
 
             return Ok(entity);
         }
 
         // POST: api/Tasks
         [HttpPost]
-        public async Task<IActionResult> CreateTask(TaskRequest taskRequest)
+        public async Task<IActionResult> CreateTask(Guid eventId, TaskRequest taskRequest)
         {
 
             var task = new EventTask
@@ -56,7 +56,7 @@ namespace EventPlanningBackend.Controllers
 
         // PUT: api/Tasks/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateTask([FromRoute][Required] Guid id, TaskRequest taskRequest)
+        public async Task<IActionResult> UpdateTask(Guid eventId, [FromRoute][Required] Guid id, TaskRequest taskRequest)
         {
             var task = new EventTask
             {
@@ -72,9 +72,9 @@ namespace EventPlanningBackend.Controllers
 
         // DELETE: api/Tasks/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTask(Guid id)
+        public async Task<IActionResult> DeleteTask(Guid eventId)
         {
-            await _taskService.DeleteTaskAsync(id);
+            await _taskService.DeleteTaskAsync(eventId);
 
             return NoContent();
         }
