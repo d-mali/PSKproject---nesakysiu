c600bdfa4b8e4fab5f4cfa414367dfac36d6502f
Daumantas Malinauskas <93260888+d-mali@users.noreply.github.com>
2024-05-25 23:39:09 +0300

    bbz changes

---
+++ Frontas/Pages/EventTaskPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontas.Pages
+{
+    public class EventTaskPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Frontas/Pages/ModTaskPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontas.Pages
+{
+    public class ModTaskPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
