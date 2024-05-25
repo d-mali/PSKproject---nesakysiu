b9ad237cdd9f96b4f8d9398d7c8665623e58883e
DeniSav <denisijus@gmail.com>
2024-05-25 20:05:35 +0300

    query

--- EventPlanningBackend/Filters/BaseQuery.cs
+++ EventPlanningBackend/Filters/BaseQuery.cs
@@ -6,16 +6,17 @@ namespace EventBackend.Filters
 {
     public class BaseQuery<T> where T : Enum
     {
-        public int Page { get; set; } = 1;
+        [DefaultValue(0)]
+        public int? Skip { get; set; }
+
+        [DefaultValue(50)]
         [Range(1, 1000)]
-        public int PageSize { get; set; } = 1000;
+        public int? Take { get; set; }
 
         public T? OrderBy { get; set; }
 
         [DefaultValue(Sorting.Asc)]
         public Sorting? Sort { get; set; }
-
-        public int ItemsToSkip() => (Page - 1) * PageSize;
     }
 
     [JsonConverter(typeof(JsonStringEnumConverter))]
