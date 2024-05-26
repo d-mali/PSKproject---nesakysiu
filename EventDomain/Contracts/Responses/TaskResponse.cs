0b047e1cd6fa46e75fca3156d7e944d21365f53e
DeniSav <denisijus@gmail.com>
2024-05-26 22:22:38 +0300

    responsas

--- EventDomain/Contracts/Responses/TaskResponse.cs
+++ EventDomain/Contracts/Responses/TaskResponse.cs
@@ -1,17 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace EventDomain.Contracts.Responses
+﻿namespace EventDomain.Contracts.Responses
 {
     public class TaskResponse
     {
         public Guid Id { get; set; }
 
-        [Required]
         public required string Title { get; set; }
 
         public DateTime? ScheduledTime { get; set; }
 
-        [Required]
         public required string Description { get; set; }
     }
 }
